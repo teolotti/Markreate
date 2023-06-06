533525acdbe2d115b71b77f90090d7756e781b02
teolo <matteo.lotti3@stud.unifi.it>
2023-06-06 18:12:18 +0200

    deploy

--- Markreate/settings.py
+++ Markreate/settings.py
@@ -128,6 +128,8 @@ STATICFILES_DIRS = [
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
